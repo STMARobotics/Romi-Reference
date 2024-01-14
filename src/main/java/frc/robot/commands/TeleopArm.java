441b9376ecbf2e977ed47a5d94c0f2278a0e27a2
Andrew Gasser <agasser01@hotmail.com>
2024-01-14 10:44:54 -0600

    Upgrade to 2024

--- src/main/java/frc/robot/commands/TeleopArm.java
+++ src/main/java/frc/robot/commands/TeleopArm.java
@@ -1,10 +1,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
 
-public class TeleopArm extends CommandBase {
+public class TeleopArm extends Command {
 
     private final XboxController controller;
     private final Arm arm;
